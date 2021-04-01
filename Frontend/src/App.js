1478784440dc5c2fc32c485a5748d281be4442d2
Anushk Gautam <me190003012@iiti.ac.in>
2021-04-01 14:15:22 +0530

    Adding backend dependencies and swipe buttons functionality

---
+++ Frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Header from "./Components/Header/Header";
+import ProfileCards from "./Components/ProfileCards/ProfileCards";
+import SwipeButtons from "./Components/SwipeButtons/SwipeButtons";
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header/>
+      <ProfileCards/>
+      <SwipeButtons/>
+      {/* Header  */}
+      {/* Image */}
+      {/* Sweap Functionality */}
+    </div>
+  );
+}
+
+export default App;
